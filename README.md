e33982903868d4c3ab70397ea167dda47d1714a9
diegogm <diegogm@amazon.com>
2018-12-18 09:06:42 -0500

    Initial Import

--- README.md
+++ README.md
@@ -1,7 +1,42 @@
-## Amazon Aurora Proxysql Example
+## Amazon Aurora ProxySQL example
 
-Amazon Aurora MySQL Compatible with ProxySQL
+A set of CloudFormation templates that demonstrates using ProxySQL middleware to provide a single endpoint for both the primary and read replica instances of an Amazon Aurora MySql-compatible cluster.
+
+These CloudFormation (CFN) templates deploy an Aurora RDS MySql-compatible
+cluster with proxysql middleware. ProxySQL offers a single endpoint that directs traffic
+to the RDS cluster endpoint for writes and the RDS reader endpoint for reads.
+
+The templates also set up a VPC with public and private subnets, security groups that 
+limit communication, and an ELB to expose the proxysql instance externally.
 
 ## License Summary
 
 This sample code is made available under a modified MIT license. See the LICENSE file.
+
+## Setup
+
+We assume that you have the AWS CLI installed and configured.  Create an S3 bucket
+to contain the CFN templates.  We'll refer to this as `templatebucket`.
+
+In the scripts `create.sh` and `update.sh`, review and update the parameters.  You must
+put in your own SSH key name in the `keyname` parameter, and we strongly encourage you to
+change the `AllowedCidrIngress` from the default of `0.0.0.0/0`.
+
+## Create the CFN stack
+
+Run:
+
+    ./create.sh templatebucket proxysql proxysqlstack us-west-2
+
+Use your AWS region of choice if you don't want to run in `us-west-2`.
+
+## Updating the stack
+
+You can run the following to update the stack if you change the configuration:
+
+    ./update.sh templatebucket proxysql proxysqlstack us-west-2
+
+## Using the stack
+
+Look for the stack output parameter `Endpoint`.  You can use `Endpoint:3306` as your
+MySql-compatible connection host.
---
+++ update.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+templatebucket=$1
+templateprefix=$2
+stackname=$3
+region=$4
+SCRIPTDIR=`dirname $0`
+if [ "$templatebucket" == "" ]
+then
+    echo "Usage: $0 <template bucket> <template prefix> <stack name> <region>"
+    exit 1
+fi
+if [ "$templateprefix" == "" ]
+then
+    echo "Usage: $0 <template bucket> <template prefix> <stack name> <region>"
+    exit 1
+fi
+if [ "$stackname" == "" ]
+then
+    echo "Usage: $0 <template bucket> <template prefix> <stack name> <region>"
+    exit 1
+fi
+if [ "$region" == "" ]
+then
+    echo "Usage: $0 <template bucket> <template prefix> <stack name> <region>"
+    exit 1
+fi
+
+# Check if we need to append region to S3 URL
+TEMPLATE_URL=https://s3.amazonaws.com/$templatebucket/$templateprefix/master.yaml
+if [ "$region" != "us-east-1" ]
+then
+    TEMPLATE_URL=https://s3-$region.amazonaws.com/$templatebucket/$templateprefix/master.yaml
+fi
+
+aws s3 cp master.yaml s3://$templatebucket/$templateprefix/master.yaml --region $region
+aws s3 cp network.yaml s3://$templatebucket/$templateprefix/network.yaml --region $region
+aws s3 cp secgroups.yaml s3://$templatebucket/$templateprefix/secgroups.yaml --region $region
+aws s3 cp aurora.yaml s3://$templatebucket/$templateprefix/aurora.yaml --region $region
+aws s3 cp proxysql.yaml s3://$templatebucket/$templateprefix/proxysql.yaml --region $region
+
+aws cloudformation update-stack --stack-name $stackname \
+    --template-url $TEMPLATE_URL \
+    --parameters \
+    ParameterKey=TemplateBucketName,ParameterValue=$templatebucket \
+    ParameterKey=TemplateBucketPrefix,ParameterValue=$templateprefix \
+    ParameterKey=ProjectTag,ParameterValue=proxysqlblog \
+    ParameterKey=vpccidr,ParameterValue="10.20.0.0/16" \
+    ParameterKey=AllowedCidrIngress,ParameterValue="0.0.0.0/0" \
+    ParameterKey=AppPrivateCIDRA,ParameterValue="10.20.3.0/24" \
+    ParameterKey=AppPrivateCIDRB,ParameterValue="10.20.4.0/24" \
+    ParameterKey=AppPrivateCIDRC,ParameterValue="10.20.5.0/24" \
+    ParameterKey=AppPrivateCIDRD,ParameterValue="10.20.6.0/24" \
+    ParameterKey=AppPublicCIDRA,ParameterValue="10.20.1.0/24" \
+    ParameterKey=AppPublicCIDRB,ParameterValue="10.20.2.0/24" \
+    ParameterKey=DatabaseName,ParameterValue="proxysqlexample" \
+    ParameterKey=DatabaseUser,ParameterValue="proxysqluser" \
+    ParameterKey=DatabasePassword,ParameterValue="pr0xySQL01Cred" \
+    ParameterKey=DbInstanceSize,ParameterValue="db.r4.large" \
+    ParameterKey=keyname,ParameterValue="ProxySQLDemo" \
+    --tags Key=Project,Value=proxysqlblog \
+    --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --no-use-previous-template \
+    --region $region
